f7493735b24ace1f719eead17199c1af34bc7d44
董建强 <1259505412@qq.com>
2016-09-09 10:54:15 +0800

    董建强版本三

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,11 +2,55 @@
 
 /**
  * @ngdoc overview
- * @name yomanApp
+ * @name yuanjinweiApp
  * @description
- * # yomanApp
+ * # yuanjinweiApp
  *
  * Main module of the application.
  */
-angular
-  .module('yomanApp', []);
+var app = angular.module('yuanjinweiApp', ['ui.router']);
+app.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('index', {
+		url: '/index',
+		templateUrl: 'views/main.html',
+		controller: 'zhuye'
+	}).state('index.zhuye', {
+		url: '/zhuye',
+		templateUrl: 'views/zhuye.html',
+	}).state('login', {
+		url: '/login',
+		templateUrl: 'views/login.html',
+		controller: 'denglu'
+	}).state('zhuce', {
+		url: '/zhuce',
+		templateUrl: 'views/register.html',
+	}).state('index.zc', {
+		url: '/zc',
+		templateUrl: 'views/zc.html',
+	}).state('index.zj', {
+		url: '/zj',
+		templateUrl: 'views/zhuan-jia1.html',
+		controller: 'z_j'
+	}).state('index.zcxx', {
+		url: '/zcxx',
+		templateUrl: 'views/zczxX.html',
+		controller: 'zc'
+	}).state('index.rencai', {
+		url: '/rencai',
+		templateUrl: 'views/rccp.html',
+	}).state('index.pxrz', {
+		url: '/pxrz',
+		templateUrl: 'views/pxrz.html',
+	}).state('index.sprz', {
+		url: '/sprz',
+		templateUrl: 'views/sprz.html',
+	}).state('index.zjzd', {
+		url: '/zjzd#1',
+		templateUrl: 'views/zhuanjiazhidao.html',
+	}).state('index.zczxS', {
+		url: '/zczxS#1',
+		templateUrl: 'views/zczxS.html',
+	})
+	$urlRouterProvider.when('', '/index/zhuye');
+
+})
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,61 @@
 
 /**
  * @ngdoc function
- * @name yomanApp.controller:MainCtrl
+ * @name yuanjinweiApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the yomanApp
+ * Controller of the yuanjinweiApp
  */
-angular.module('yomanApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('yuanjinweiApp')
+	.controller('zhuye', function($scope, $http, $state) {
+		$scope.dl = function() {
+			$state.go('login')
+		}
+		$scope.zc = function() {
+			$state.go('index.zc')
+		}
+		$scope.zhuanjia = function() {
+			$state.go('index.zj')
+		}
+		$scope.sy = function() {
+			$state.go('index.zhuye')
+		}
+		$scope.zcxx = function() {
+			$state.go('index.zcxx')
+		}
+		$scope.rencai = function() {
+			$state.go('index.rencai')
+		}
+		$scope.pxrz = function() {
+			$state.go('index.pxrz')
+		}
+		$scope.sprz = function() {
+			$state.go('index.sprz')
+		}
+		$('#znav ul li').hover(function() {
+			$(this).find('ol').stop().animate({
+				width: "100%",
+				height: $(this).find("ol").children().length * 100 + '%',
+			}, 200)
+		}, function() {
+			$(this).find('ol').stop().animate({
+				width: "100%",
+				height: "0%",
+			}, 200)
+		})
+	})
+	.controller('denglu', function($scope, $http, $state) {
+		$scope.zhuce = function() {
+			$state.go('zhuce')
+		}
+	})
+	.controller('z_j', function($scope, $http, $state) {
+		$scope.zjzd = function() {
+			$state.go('index.zjzd')
+		}
+	})
+	.controller('zc', function($scope, $http, $state) {
+		$scope.zc = function() {
+			$state.go('index.zczxS')
+		}
+	})
